8520d1068b221ca32b0a590ca2407e1975446bad
Akuya <aiyuegonzalbes@gmail.com>
2022-10-05 09:30:58 +0200

    Trigger_Cinematica

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerMovement Controller;
     private Rigidbody2D rb;
-    public float speed = 5.5f;
+    float dirX;
+    public float speed = 5f;
     private float horizontal;
+    public PlayableDirector director;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -16,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate() 
     {
         //la velocidad del Rigidbody es un vector que en el eje X, mueves en horizontal dependiendo de la velocidad(multiplica)
-        rb.velocity = new Vector2 (horizontal * speed, 0f);
+        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
     }
 
     // Update is called once per frame
@@ -27,5 +32,13 @@ public class PlayerMovement : MonoBehaviour
         //playerTransform.position += new Vector3 (horizontal * speed * Time.deltaTime, 0 , 0);
         //playerTransform.position += new Vector3 (1, 0 , 0) * horizontal * speed * Time.deltaTime;
         //playerTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "Cinematica")
+        {
+           director.Play();
+        }
     }
 }

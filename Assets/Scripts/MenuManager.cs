59f10947dc829b94ebf7c888a81fafe190bc8de3
Akuya <aiyuegonzalbes@gmail.com>
2022-10-26 11:31:03 +0200

    Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public GameObject title;
+	public GameObject mainButtons;
+	public GameObject optionsMenu;
+	
+
+	public void OpenOptios()
+	{
+		title.SetActive(false);
+		mainButtons.SetActive(false);
+		optionsMenu.SetActive(true);
+	}
+    
+}

7fa0f9f65c1e47286b318df3db1987a5b1696f95
Akuya <aiyuegonzalbes@gmail.com>
2022-11-09 12:24:22 +0100

    Loop

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -23,7 +23,7 @@ public class CameraShake : MonoBehaviour
         Vector3 originalPos = transform.position;
         float elapsed = 0f;
 
-        while(elapsed < duration)
+        /*while(elapsed < duration)
         {
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
@@ -31,6 +31,34 @@ public class CameraShake : MonoBehaviour
             transform.position = new Vector3(x + originalPos.x, y + originalPos.y, transform.position.z);
             elapsed += Time.deltaTime;
             yield return 0;
-        }
+        }*/
+
+        /*for(float i = elapsed; i < duration; i += Time.deltaTime)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x + originalPos.x, y + originalPos.y, transform.position.z);
+            yield return 0;
+        }*/
+
+        do
+        {
+            Debug.Log("shake");
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x + originalPos.x, y + originalPos.y, transform.position.z);
+            elapsed += Time.deltaTime;
+            yield return 0;
+
+        }while(elapsed < duration);
+
+        /*GameObject[] vidas; 
+
+        foreach(GameObject vida in vidas)
+        {
+            vida.SetActive(false);
+        }*/
    }
 }

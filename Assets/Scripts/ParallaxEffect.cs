de677308db2067f866f9bab56f4690aab096aee6
Akuya <aiyuegonzalbes@gmail.com>
2022-11-16 10:15:12 +0100

    bg

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -9,16 +9,37 @@ public class ParallaxEffect : MonoBehaviour
     private Transform cameraTransform;
     private Vector3 cameraPreviousPos;
 
+    private float spriteWidth;
+    private float startPos;
+
     void Start()
     {
         cameraTransform = Camera.main.transform;
         cameraPreviousPos = cameraTransform.position;
+
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        float x = cameraTransform.position.x - cameraPreviousPos.x;
+        float x = (cameraTransform.position.x - cameraPreviousPos.x) * ParallaxMultiplier;
+        float spriteMoveAmount = cameraTransform.position.x * (1 - ParallaxMultiplier);
+
         transform.Translate(new Vector3(x, 0f, 0f));
+        cameraPreviousPos = cameraTransform.position;
+
+        if(spriteMoveAmount > startPos + spriteWidth)
+        {
+            transform.Translate(new Vector3(spriteWidth, 0f, 0f));
+            startPos += spriteWidth;
+        }
+
+        else if(spriteMoveAmount < startPos - spriteWidth)
+        {
+            transform.Translate(new Vector3(-spriteWidth, 0f, 0f));
+            startPos -= spriteWidth;
+        }
     }
 }
